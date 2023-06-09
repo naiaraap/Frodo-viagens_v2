95c43a2367361f3e5e9573e00f9cd6e1c7e78158
Naiara Pantuza <naiarapantuza@gmail.com>
2023-06-09 10:21:32 -0300

    criacão de pasta highlights. offers, arquivo para table viewcell + XIB para novo modelo de célula offers

---
+++ FrodoViagens/FrodoViagens/View/Cells/Offers/OffersTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  OffersTableViewCell.swift
+//  FrodoViagens
+//
+//  Created by Naiara de Almeida Pantuza on 09/06/23.
+//
+
+import UIKit
+
+class OffersTableViewCell: UITableViewCell {
+
+    
+}
