3a1776e74b12df167e97d11300f7fbbd9b196371
Naiara Pantuza <naiarapantuza@gmail.com>
2023-06-12 14:35:33 -0300

    setting IBOutlets for trip cell

--- FrodoViagens/FrodoViagens/View/Cells/Offers/OffersTableViewCell.swift
+++ FrodoViagens/FrodoViagens/View/Cells/Offers/OffersTableViewCell.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 class OffersTableViewCell: UITableViewCell {
+  
+  //MARK: - IBOutlets
+  
+  @IBOutlet var offerTripImageView: [UIImageView]!
+  @IBOutlet var offerTripTitleLabel: [UILabel]!
+  @IBOutlet var offerTripSubtitleLabel: [UILabel]!
+  @IBOutlet var offerTripFullPriceLabel: [UILabel]!
+  @IBOutlet var offerTripPriceWithDiscountLabel: [UILabel]!
+  
   func setCell(_ trip: Trip?) {
     
   }
