54201ad845a44b7d82e15a31460b0008277524cd
Naiara Pantuza <naiarapantuza@gmail.com>
2023-06-14 16:15:23 -0300

    outlets and action for detail back button

--- FrodoViagens/FrodoViagens/View/DetailViewController/DetailViewController.swift
+++ FrodoViagens/FrodoViagens/View/DetailViewController/DetailViewController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
+  //MARK: - Outlets
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var titleView: UILabel!
+  @IBOutlet weak var SubtitleView: UILabel!
+  
+  //MARK - View Life Cicle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    //setupView()
+  }
+  
+  //MARK - Actions
+  @IBAction func tapOnBackButton() {
+    navigationController?.popViewController(animated: true)
+  }
+  
 }
